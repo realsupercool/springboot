c64097de733117445f1b950d8f3518125fb9ef22
GeckosRoam <qxd_96@163.com>
2019-07-04 11:51:57 +0800

    springboot

--- producers/src/main/java/com/qphone/producers/controller/HelloController.java
+++ producers/src/main/java/com/qphone/producers/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-
+//www
     @RequestMapping("hello/{type}")
     public String providerService(@PathVariable String type){
         return type+":我是一个暴露的服务";
